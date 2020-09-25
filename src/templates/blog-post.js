9982f3a230917fd5cbc190ae5a415a62e4511cc3
markrall <62776921+markrall@users.noreply.github.com>
2020-09-25 19:32:22 +1000

    Fixed Bio component association

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,7 @@ import { MDXRenderer } from "gatsby-plugin-mdx"
 import Image from "gatsby-image"
 
 import { useSiteMetadata } from "../hooks/useSiteMetadata"
-import Bio from "../components/Bio/bio"
+import Bio from '../components/Bio/Bio'
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/SEO/seo"
 import { rhythm } from "../utils/typography"
